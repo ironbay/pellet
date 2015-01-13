db5b3a4e1e63a59f0e8f361580848f5311988b52
dax <me@ly.ht>
2015-01-13 14:40:22 -0500

    Initial commmit

---
+++ io.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func copyFile(source string, dest string) (err error) {
+	sourcefile, err := os.Open(source)
+	if err != nil {
+		return err
+	}
+
+	defer sourcefile.Close()
+
+	destfile, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+
+	defer destfile.Close()
+
+	_, err = io.Copy(destfile, sourcefile)
+	if err == nil {
+		sourceinfo, err := os.Stat(source)
+		if err != nil {
+			err = os.Chmod(dest, sourceinfo.Mode())
+		}
+
+	}
+
+	return
+}
+
+func copyDir(source string, dest string) (err error) {
+
+	// get properties of source dir
+	sourceinfo, err := os.Stat(source)
+	if err != nil {
+		return err
+	}
+
+	// create dest dir
+
+	os.RemoveAll(dest)
+
+	err = os.MkdirAll(dest, sourceinfo.Mode())
+	if err != nil {
+		return err
+	}
+
+	directory, _ := os.Open(source)
+
+	objects, err := directory.Readdir(-1)
+
+	for _, obj := range objects {
+		if obj.Name() == "compiled" {
+			continue
+		}
+		sourcefilepointer := source + "/" + obj.Name()
+
+		destinationfilepointer := dest + "/" + obj.Name()
+
+		if obj.IsDir() {
+			// create sub-directories - recursively
+			err = copyDir(sourcefilepointer, destinationfilepointer)
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			// perform copy
+			err = copyFile(sourcefilepointer, destinationfilepointer)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+
+	}
+	return
+}
---
+++ pellet.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/dchest/cssmin"
+	"github.com/dchest/jsmin"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	source, _ := os.Getwd()
+	output := path.Join(source, "compiled")
+	version := strconv.FormatInt(time.Now().Unix(), 10)
+	fmt.Println("Source Directory")
+	fmt.Println("   ", source)
+	fmt.Println("Output Directory")
+	fmt.Println("    ", output)
+	copyDir(source, output)
+
+	directory, _ := os.Open(output)
+	defer directory.Close()
+
+	files, _ := directory.Readdir(-1)
+	for _, f := range files {
+		if !strings.HasSuffix(f.Name(), ".html") {
+			continue
+		}
+
+		fmt.Println("Parsing", f.Name())
+		full := path.Join(directory.Name(), f.Name())
+		handle, _ := os.Open(full)
+
+		doc, _ := goquery.NewDocumentFromReader(handle)
+
+		{
+			fmt.Println("    Parsing Styles...")
+			var css bytes.Buffer
+			doc.Find("link[rel='stylesheet']").Each(func(i int, s *goquery.Selection) {
+				href, exists := s.Attr("href")
+				if !exists {
+					return
+				}
+				fmt.Println("        ->", href)
+				if strings.HasPrefix(href, "http") {
+					return
+				}
+				contents, _ := ioutil.ReadFile(path.Join(directory.Name(), href))
+				css.WriteString(string(contents))
+				s.Remove()
+			})
+			mini := cssmin.Minify(css.Bytes())
+			o := "/css/" + f.Name() + "." + version + ".css"
+			ioutil.WriteFile(path.Join(output, o), mini, f.Mode())
+			doc.Find("head").AppendHtml("<link href='" + o + "' rel='stylesheet'/>")
+		}
+
+		{
+			fmt.Println("    Parsing Scripts...")
+			var js bytes.Buffer
+			doc.Find("script").Each(func(i int, s *goquery.Selection) {
+				if _, exists := s.Attr("debug"); exists {
+					s.Remove()
+					return
+				}
+				href, exists := s.Attr("src")
+				if !exists {
+					return
+				}
+				fmt.Println("        ->", href)
+				if strings.HasPrefix(href, "http") {
+					return
+				}
+				contents, _ := ioutil.ReadFile(path.Join(directory.Name(), href))
+				if t, exists := s.Attr("type"); exists && t == "text/jsx" {
+					c := exec.Command("jsx")
+					stdin, _ := c.StdinPipe()
+					c.Stdout = os.Stdout
+					c.Start()
+					io.WriteString(stdin, string(contents))
+					c.Wait()
+					stdin.Close()
+
+				}
+				js.WriteString(string(contents))
+				s.Remove()
+			})
+			mini, _ := jsmin.Minify(js.Bytes())
+			o := "/js/" + f.Name() + "." + version + ".js"
+			ioutil.WriteFile(path.Join(output, o), mini, f.Mode())
+			doc.Find("head").AppendHtml("<script src='" + o + "' />")
+		}
+
+		handle.Close()
+		html, _ := doc.Html()
+		ioutil.WriteFile(full, []byte(html), f.Mode())
+	}
+
+}
