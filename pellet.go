7ffdc9fc87d562b2c90d09a76f4eaadcc1b85dea
dax <me@ly.ht>
2015-01-13 15:14:29 -0500

    Appned scripts to end

--- pellet.go
+++ pellet.go
@@ -91,7 +91,7 @@ func main() {
 				s.Remove()
 			})
 			mini, _ := jsmin.Minify(js.Bytes())
-			mini = js.String()
+			mini = js.Bytes()
 			o := "/js/" + f.Name() + "." + version + ".js"
 			ioutil.WriteFile(path.Join(output, o), mini, f.Mode())
 			doc.Find("body").AppendHtml("<script src='" + o + "' />")
